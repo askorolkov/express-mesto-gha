5615a037f99e68c08c99179e36be8794d7ff4a6b
Aleksey Korolkov <askorolkov1@gmail.com>
2023-05-07 11:23:32 +0500

    post review fix

--- routes/index.js
+++ routes/index.js
@@ -1,4 +1,5 @@
 const router = require('express').Router();
+const STATUS_CODE = require('../errors/errors');
 
 const usersRouter = require('./users');
 const cardsRouter = require('./cards');
@@ -7,7 +8,7 @@ router.use(usersRouter);
 router.use(cardsRouter);
 
 router.use('/*', (req, res) => {
-  res.status(404).send({ message: 'Page not found' });
+  res.status(STATUS_CODE.NOT_FOUND).send({ message: 'Page not found' });
 });
 
 module.exports = router;
